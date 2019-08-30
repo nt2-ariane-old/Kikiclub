d34ef05b83c4bd5182197948b49a757f6fc2e40d
Ludovic Doutre-GUay <ludovicdguay@gmail.com>
2019-08-30 15:42:28 -0400

    test

--- ajax/login-ajax.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/action/Ajax/LoginAjaxAction.php");
-	header('Access-Control-Allow-Origin: https://kikinumerique.wixsite.com/kikiclubsandbox');
-
-	$action = new LoginAjaxAction();
-	$action->execute();
-
-	echo json_encode($action->results);
